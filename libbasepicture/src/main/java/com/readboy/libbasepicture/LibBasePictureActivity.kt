512786ae8f83d253efe213e466d446c710bcd0f9
Liu JiaWei <l1uj1awe1@sina.com>
2018-08-20 15:08:58 +0800

    init libbasepicture

---
+++ libbasepicture/src/main/java/com/readboy/libbasepicture/LibBasePictureActivity.kt
@@ -0,0 +1,13 @@
+package com.readboy.libbasepicture
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.readboy.libbaseui.R
+
+class LibBasePictureActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.libbasepicture_activity_lib_base_picture)
+    }
+}
