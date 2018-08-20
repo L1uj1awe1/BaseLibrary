8e6ca91b770fc3dacc8a0ee30a5b4640ddd0bf5c
Liu JiaWei <l1uj1awe1@sina.com>
2018-08-20 16:00:34 +0800

    init database

---
+++ libbasedatabase/src/main/java/com/lreadboy/ibbasedatabase/BaseDatabaseActivity.kt
@@ -0,0 +1,13 @@
+package com.lreadboy.ibbasedatabase
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.readboy.ibbasedatabase.R
+
+class BaseDatabaseActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.libbasedatabase_activity_base_database)
+    }
+}
