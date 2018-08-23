9e0de288130906e8c6af61992e72a001ae6cdc3c
Liu JiaWei <l1uj1awe1@sina.com>
2018-08-23 16:16:32 +0800

    优化原生HttpManager交互

--- libbasenetwork/src/main/java/com/readboy/ibbasenetwork/LibBaseNetworkActivity.kt
+++ libbasenetwork/src/main/java/com/readboy/ibbasenetwork/LibBaseNetworkActivity.kt
@@ -12,7 +12,7 @@ import kotlinx.android.synthetic.main.libbasenetwork_activity_lib_base_network.*
 
 // todo 不知道网络基础库，对于不同header怎么处理，无从下手
 // todo 不知道，那些才是最基础的网络依赖库，例如retrofit来说，无从下手
-class LibBaseNetworkActivity : AppCompatActivity(), HttpManager.HttpListener {
+class LibBaseNetworkActivity : AppCompatActivity(), HttpManager.HttpCallback {
 
     private val TAG = "LibBaseNetworkActivity"
 
@@ -32,10 +32,10 @@ class LibBaseNetworkActivity : AppCompatActivity(), HttpManager.HttpListener {
      * http 使用例子
      */
     private fun requestForHttp() {
-        mHttpManager = HttpManager(
+        mHttpManager = HttpManager()
+        mHttpManager?.execute(
                 "http://www.zhbuswx.com/RealTime/GetRealTime?id=B9&fromStation=前山总站&_=${System.currentTimeMillis()}",
                 this)
-        mHttpManager?.execute()
     }
     override fun onHttpRequestResult(result: String?) {
         Log.e(TAG, "onHttpRequestResult  result = " + result)
--- libbasenetwork/src/main/java/com/readboy/ibbasenetwork/http/HttpManager.kt
+++ libbasenetwork/src/main/java/com/readboy/ibbasenetwork/http/HttpManager.kt
@@ -10,22 +10,28 @@ import android.os.AsyncTask
  * @param listener 网络连接监听，需要继承重写回调接口
  *
  */
-class HttpManager(private val url: String, private val listener: HttpListener? = null) {
+class HttpManager {
 
     private var task: HttpAsyncTask? = null
+    private var mUrl = ""
+    private var listener: HttpCallback? = null
 
-    interface HttpListener {
+    interface HttpCallback {
         fun onHttpRequestResult(result: String?)
     }
 
     /**
-     * 开始请求
+     * @aim 开始请求
+     *
+     * @param url 请求连接，需要自行拼接请求参数
+     * @param callback 网络连接监听，需要继承重写回调接口
      */
-    fun execute() {
-        if (task != null && task?.status == AsyncTask.Status.RUNNING) {
-            task?.cancel(true)
-            task = null
-        }
+    fun execute(url: String, callback: HttpCallback? = null) {
+        mUrl = url
+        listener = callback
+
+        cancel()
+
         task = HttpAsyncTask().execute() as? HttpAsyncTask
     }
 
@@ -43,7 +49,7 @@ class HttpManager(private val url: String, private val listener: HttpListener? =
     inner class HttpAsyncTask: AsyncTask<String, Int, String>() {
 
         override fun doInBackground(vararg params: String?): String? {
-            return HttpConnectManager.connectResult(url)
+            return HttpConnectManager.connectResult(mUrl)
         }
 
         override fun onPostExecute(result: String?) {
