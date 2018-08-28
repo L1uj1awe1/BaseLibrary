c105e62af89c6afa850c4ee136da13ae27692274
Liu JiaWei <l1uj1awe1@sina.com>
2018-08-28 17:57:21 +0800

    reanem

--- libbaseui/src/main/java/com/readboy/libbaseui/BaseUIActivity.kt
+++ libbaseui/src/main/java/com/readboy/libbaseui/BaseUIActivity.kt
@@ -2,12 +2,11 @@ package com.readboy.libbaseui
 
 import android.os.Bundle
 import com.readboy.libbaseui.activity.SwipeBackActivity
-import com.readboy.libbaseui.bar.BarHelper
 
 class BaseUIActivity : SwipeBackActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.libbaseui_activity_base_ui)
+        setContentView(R.layout.ui_activity_base_ui)
     }
 }
