ec525c724c60adb5fdd881dd1a0c2dc8cf5296f7
Liu JiaWei <l1uj1awe1@sina.com>
2018-08-28 20:21:51 +0800

    toast

--- libbaseui/src/main/java/com/readboy/libbaseui/BaseUIActivity.kt
+++ libbaseui/src/main/java/com/readboy/libbaseui/BaseUIActivity.kt
@@ -2,11 +2,17 @@ package com.readboy.libbaseui
 
 import android.os.Bundle
 import com.readboy.libbaseui.activity.SwipeBackActivity
+import com.readboy.libbaseui.toast.ToastHelper
+import kotlinx.android.synthetic.main.ui_activity_base_ui.*
 
 class BaseUIActivity : SwipeBackActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.ui_activity_base_ui)
+
+        btn.setOnClickListener {
+            ToastHelper.show(this, "aaaaaa")
+        }
     }
 }
---
+++ libbaseui/src/main/java/com/readboy/libbaseui/toast/ToastHelper.kt
@@ -0,0 +1,45 @@
+package com.readboy.libbaseui.toast
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.widget.Toast
+
+/**
+ * Created by liujiawei on 18-8-28.
+ */
+object ToastHelper {
+
+    private var mToast: Toast? = null
+
+    @SuppressLint("ShowToast")
+    fun show(context: Context, msg: String, delay: Int = Toast.LENGTH_SHORT) {
+        try {
+            if (mToast != null) {
+                mToast?.cancel()
+                mToast = null
+                mToast = Toast.makeText(context, msg, delay)
+            } else {
+                mToast = Toast.makeText(context, msg, delay)
+            }
+            mToast?.show()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    @SuppressLint("ShowToast")
+    fun show(context: Context, msg: Int, delay: Int = Toast.LENGTH_SHORT) {
+        try {
+            if (mToast != null) {
+                mToast?.cancel()
+                mToast = null
+                mToast = Toast.makeText(context, msg, delay)
+            } else {
+                mToast = Toast.makeText(context, msg, delay)
+            }
+            mToast?.show()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+}
