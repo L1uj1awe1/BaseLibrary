3fb0d508d508b2e324b74e80256fe61ba9d9460b
Liu JiaWei <l1uj1awe1@sina.com>
2018-12-12 21:08:06 +0800

    UI 阻塞 检测

---
+++ libbasecomponent/src/main/java/com/readboy/libbasecomponent/block/LogMonitor.kt
@@ -0,0 +1,51 @@
+package com.readboy.libbasecomponent.block
+
+import android.os.Handler
+import android.os.HandlerThread
+import android.os.Looper
+import android.util.Log
+
+class LogMonitor {
+
+    /**
+     * 卡顿触发的时间阈值
+     */
+    private val TIME_BLOCK = 1000L
+
+    private val mLogThread = HandlerThread("log")
+    private val mIoHandler: Handler
+
+    /**
+     * 打印当前线程执行位置，即 UI 卡顿的位置
+     */
+    private val mLogRunnable = Runnable {
+        val sb = StringBuilder()
+        val stackTrace = Looper.getMainLooper().thread.stackTrace
+        for (s in stackTrace) {
+            sb.append(s.toString() + "\n")
+        }
+        Log.e("UIBlockAnalysis", sb.toString())
+    }
+
+    init {
+        mLogThread.start()
+        mIoHandler = Handler(mLogThread.looper)
+    }
+
+    fun startMonitor() {
+        mIoHandler.postDelayed(mLogRunnable, TIME_BLOCK)
+    }
+
+    fun removeMonitor() {
+        mIoHandler.removeCallbacks(mLogRunnable)
+    }
+
+    companion object {
+
+        fun get(): LogMonitor {
+            return LogMonitor()
+        }
+
+        val instance: LogMonitor = get()
+    }
+}
---
+++ libbasecomponent/src/main/java/com/readboy/libbasecomponent/block/UIBlockAnalysis.kt
@@ -0,0 +1,43 @@
+package com.readboy.libbasecomponent.block
+
+import android.os.Looper
+
+/**
+ * UI 阻塞检查
+ *
+ * https://blog.csdn.net/lmj623565791/article/details/58626355
+ *
+ * Application -> onCreate() 初始化
+ *
+ * if (BuildConfig.DEBUG) {
+ *     UIBlockAnalysis.start()
+ *  }
+ */
+object UIBlockAnalysis {
+
+    private val TAG_START = ">>>>> Dispatching"
+    private val TAG_END = "<<<<< Finished"
+
+    fun start() {
+
+        /**
+         * Android UI线程中有个Looper，在其loop方法中会不断取出Message，调用其绑定的Handler在UI线程进行执行
+         *
+         * 执行开始 打印 >>>>> Dispatching
+         * 执行结束 打印 <<<<< Finished
+         *
+         * 检测到开始时候，开启指定时间的延迟打印
+         * 如果在延迟时间内，loop()方法还未执行到结束打印 <<<<< Finished的步骤，则会输出自定义打印，找到 UI 卡顿的地方
+         */
+        Looper.getMainLooper().setMessageLogging { x ->
+
+            if (x.startsWith(TAG_START)) {
+                LogMonitor.instance.startMonitor()
+            }
+            if (x.startsWith(TAG_END)) {
+                LogMonitor.instance.removeMonitor()
+            }
+        }
+
+    }
+}
