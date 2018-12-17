012affbcc16ba8ff95edfee34f7b67b7e1a0ee15
Liu JiaWei <l1uj1awe1@sina.com>
2018-12-17 17:52:07 +0800

    增加 Document

--- libbasecomponent/src/main/java/com/readboy/libbasecomponent/BaseComponentActivity.kt
+++ libbasecomponent/src/main/java/com/readboy/libbasecomponent/BaseComponentActivity.kt
@@ -3,17 +3,6 @@ package com.readboy.libbasecomponent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-/**
- * 基础组件库
- *
- * 日志库    https://github.com/orhanobut/logger
- *
- * 权限管理库    https://github.com/tbruyelle/RxPermissions
- *
- * 工具库    https://github.com/Blankj/AndroidUtilCode
- *
- */
-
 class BaseComponentActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
